56d626ae65d441d1c704816e499f345fdd8365fa
Rowan Polster <rowandownriver94@gmail.com>
2021-07-19 19:20:49 -0500

    Add project files.

---
+++ Chapter3-Studio/CharacterCounting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter3_Studio
+{
+    public class CharacterCounting
+    {
+        public void findChars(char[] array) 
+        {
+            var characters = new List<char>();
+
+            foreach (char c in array)
+            {
+                if (!characters.Contains(c))
+                {
+                    characters.Add(c);
+                }
+            }
+            
+            foreach(char character in characters)
+            {
+                Console.WriteLine(character);
+            }
+        }
+
+        public void numberOfChars(string sentence)
+        {
+            var charCounts = new List<int>();
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                int count = sentence.ToLower().Split(sentence[i]).Length - 1;
+
+                if (!charCounts.Contains(count))
+                {
+                    charCounts.Add(count);
+                }
+            }
+
+            foreach (int charCount in charCounts)
+            {
+                Console.WriteLine(charCount);
+            }
+        }
+    }
+}
---
+++ Chapter3-Studio/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter3_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "this is a practice sentence";
+            char[] charactersInString = sentence.ToLower().ToCharArray();
+
+            
+
+
+
+            //var characters = new List<char>();
+            var charCounts = new List<int>();
+
+
+            // foreach (char c in charactersInString)
+            //{
+            //if (!characters.Contains(c))
+            //{
+            //characters.Add(c);
+            //}
+            //}
+
+            //for (int i = 0; i < charactersInString.Length; i++)
+            //{
+                //int count = rumi.ToLower().Split(rumi[i]).Length;
+
+                //if (!charCounts.Contains(count))
+                //{
+                    //charCounts.Add(count);
+                //}
+            //}
+
+
+
+
+
+            //CharacterCounting chars = new CharacterCounting();
+
+
+            //chars.findChars(charactersInString);
+            //chars.numberOfChars(rumi);
+            //foreach(char character in rumi)
+            {
+                //var dictionary = rumi.ToDictionary(key => charactersInString[character], value => charCounts);
+                // foreach(var pair in dictionary)
+                // {
+                //     Console.WriteLine(pair.Key, pair.Value)
+                //}
+                //}
+
+                var dictionary = new Dictionary<char, int>();
+
+                for (int i = 0; i < charactersInString.Length; i++)
+                {
+
+                    int count = sentence.Split(sentence[i]).Length;
+                    char characters = charactersInString[i];
+
+                    if (!dictionary.ContainsKey(characters) && !dictionary.ContainsValue(count -1))
+                    {
+                        dictionary.Add(characters, count -1);
+                    }
+
+
+
+                }
+
+                foreach(char character in charactersInString)
+                {
+                    int count = sentence.ToLower().Split(character).Length;
+                    if (!dictionary.ContainsKey(character))
+                    {
+                        dictionary.Add(character, count);
+                    }
+                }
+
+                foreach(var pair in dictionary)
+                {
+                    Console.WriteLine("KEY {0} VALUE {1}", pair.Key, pair.Value);
+                }
+
+
+
+
+
+
+
+            }
+
+        }
+    }
+}
+
