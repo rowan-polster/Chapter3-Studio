0a8e96b17a29e2b83e4f26c84c3ead7093469f7b
Rowan Polster <rowandownriver94@gmail.com>
2021-07-19 22:03:24 -0500

    got rid of some commented-out code

--- Chapter3-Studio/Program.cs
+++ Chapter3-Studio/Program.cs
@@ -9,53 +9,10 @@ namespace Chapter3_Studio
         static void Main(string[] args)
         {
             var sentence = "this is a practice sentence";
-            char[] charactersInString = sentence.ToLower().ToCharArray();
+            char[] charactersInString = sentence.ToLower().ToCharArray(); 
+            var dictionary = new Dictionary<char, int>();
 
-            
-
-
-
-            //var characters = new List<char>();
-            var charCounts = new List<int>();
-
-
-            // foreach (char c in charactersInString)
-            //{
-            //if (!characters.Contains(c))
-            //{
-            //characters.Add(c);
-            //}
-            //}
-
-            //for (int i = 0; i < charactersInString.Length; i++)
-            //{
-                //int count = rumi.ToLower().Split(rumi[i]).Length;
-
-                //if (!charCounts.Contains(count))
-                //{
-                    //charCounts.Add(count);
-                //}
-            //}
-
-
-
-
-
-            //CharacterCounting chars = new CharacterCounting();
-
-
-            //chars.findChars(charactersInString);
-            //chars.numberOfChars(rumi);
-            //foreach(char character in rumi)
-            {
-                //var dictionary = rumi.ToDictionary(key => charactersInString[character], value => charCounts);
-                // foreach(var pair in dictionary)
-                // {
-                //     Console.WriteLine(pair.Key, pair.Value)
-                //}
-                //}
-
-                var dictionary = new Dictionary<char, int>();
+            { 
 
                 for (int i = 0; i < charactersInString.Length; i++)
                 {
